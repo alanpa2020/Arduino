5d7a3d811188c8a01efcc36fca89ec24f5fc4ebb
alanpa2020 <61548019+alanpa2020@users.noreply.github.com>
2020-03-12 20:18:38 +0300

    Робот пошел.
    7 прокачек - 10 шагов - 7 прокачек - 10 шагов....

--- steper/steper.ino
+++ steper/steper.ino
@@ -2,37 +2,51 @@
 Servo servoRigthBedro, servoRightGolen, servoRightStopa; 
 Servo servoLeftBedro, servoLeftGolen, servoLeftStopa; 
 
-const int positionStensd [6] = {70, 90, 120, 100, 105, 20};
+//базовое положение
+const double positionStensd [6] = {55, 90, 120, 115, 105, 20};
 
-void setPosition (int pos [6]);
-void moveToPosition (int pos [6]); 
+double nullPosition [6] = {0, 0, 0, 0, 0, 0};
+
+//разбиение шага
+const int defferenc = 40;
+
+
+void setPosition (double pos [6]);
+void moveToPositionVector (double pos [6]); 
+void stepLeft ();
 
 void setup()
 {
-   servoRigthBedro.attach(3);
-   servoRightGolen.attach(5);
-   servoRightStopa.attach(6);
-   servoLeftBedro.attach(9);
-   servoLeftGolen.attach(10);
-   servoLeftStopa.attach(11);
+   servoRigthBedro.attach (3);
+   servoRightGolen.attach (5);
+   servoRightStopa.attach (6);
+   servoLeftBedro.attach  (9);
+   servoLeftGolen.attach  (10);
+   servoLeftStopa.attach  (11);
    setPosition (positionStensd);
-}
- 
-void loop()
-{
-   setPosition (positionStensd);
-   delay (500);
-   int position [6] = {30, 0, 0, 30, 0, 0};
-   moveToPosition (position);
-   delay (500);
-   for (int i = 0; i < 6; i++) {
-      position [i] = 0;
-   }
-   moveToPosition (position);
+   
+   Serial.begin(9600);
    
 }
 
-void setPosition (int pos [6]){
+//================================================================================
+void loop(){
+   double position [6] = {30, 30, 30, 30, 30, 30};
+   
+   for (int i = 1; i<7; i++) {
+      moveToPositionVector (position);
+      delay (100);
+
+      moveToPositionVector (nullPosition);
+      delay (100);
+   }
+
+   for (int i = 1; i<10; i++) {
+      stepLeft();
+   }  
+}
+//================================================================================
+void setPosition (double pos [6]){
    servoRigthBedro.write(pos[0]);
    servoRightGolen.write(pos[1]);
    servoRightStopa.write(pos[2]);
@@ -41,12 +55,94 @@ void setPosition (int pos [6]){
    servoLeftStopa.write(pos[5]);
 }
 
-void moveToPosition (int pos [6]){
-  int naturalEngle [6] = {positionStensd[0]-pos[0], 
-                          positionStensd[1]-pos[1], 
-                          positionStensd[2]-pos[2], 
-                          positionStensd[3]+pos[3], 
-                          positionStensd[4]+pos[4], 
-                          positionStensd[5]+pos[5]};
-  setPosition (naturalEngle);
+void moveToPositionVector (double pos [6]){
+  double targetEngle [6] = {   positionStensd[0]+pos[0], 
+                              positionStensd[1]+pos[1], 
+                              positionStensd[2]-pos[2], 
+                              positionStensd[3]-pos[3], 
+                              positionStensd[4]-pos[4], 
+                              positionStensd[5]+pos[5]};
+  double startEngle [6] = {   servoRigthBedro.read(),
+                              servoRightGolen.read(),
+                              servoRightStopa.read(),
+                              servoLeftBedro.read(),
+                              servoLeftGolen.read(),
+                              servoLeftStopa.read()};
+  
+  double microStep [6] = {0};
+  for (int i = 0; i<6; i++) {
+      microStep [i] = (targetEngle[i] - startEngle[i]) / defferenc;
+  }
+  
+  for (int i = 0; i<defferenc; i++) {
+      for (int serva = 0; serva<6; serva++) {
+          startEngle[serva] =  startEngle[serva] + microStep [serva];
+          //Serial.print (startEngle[serva]);
+          //Serial.print (" ");
+      }
+    
+      setPosition (startEngle);
+      delay(10);  // SPEED----------------------------------------------
+  }
+  
+  setPosition (targetEngle);
 }
+
+//------------------------------------------------------------------------
+void stepLeft () {
+    double position [6] = {0, 0, 20, 30, 30, -20};
+    moveToPositionVector (position);
+    position [0] = 80;
+    position [1] = 30;
+    moveToPositionVector (position);
+    //delay(1000);
+    position [3] = 0;
+    position [4] = 0;
+    moveToPositionVector (position);
+    //delay(1000);
+    position [3] = -20;
+    position [4] = -20;
+    moveToPositionVector (position);
+    //delay(1000);
+    position [0] = 30;
+    position [1] = 30;
+    position [2] = 0;
+    position [3] = -20;
+    position [4] = -20;
+    position [5] = 0;
+    moveToPositionVector (position);
+    //delay(1000);
+    position [2] = -20;
+    position [5] = 20;
+    moveToPositionVector (position);
+    //delay(1000);
+    
+    
+    //------------
+    moveToPositionVector (position);
+    position [3] = 80;
+    position [4] = 30;
+    moveToPositionVector (position);
+    //delay(1000);
+    position [0] = 0;
+    position [1] = 0;
+    moveToPositionVector (position);
+    //delay(1000);
+    position [0] = -20;
+    position [1] = -20;
+    moveToPositionVector (position);
+    //delay(1000);
+    position [3] = 30;
+    position [4] = 30;
+    position [5] = 0;
+    position [0] = -20;
+    position [1] = -20;
+    position [2] = 0;
+    moveToPositionVector (position);
+    //delay(1000);
+    position [5] = -20;
+    position [2] = 20;
+    moveToPositionVector (position);
+    delay(1000);  
+}
+//------------------------------------------------------------------------
